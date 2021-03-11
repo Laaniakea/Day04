f789830ec6272bf08d3de67d58afac1ee582c179
unknown <edina.kurtic90@hotmail.com>
2021-03-11 22:27:23 +0100

    Dodan ex07

---
+++ ex07/src/index.js
@@ -0,0 +1,12 @@
+function main (numOne, numTwo){
+    //only change code below this line
+    
+    var remainder = numOne % numTwo;
+
+    //only change code below this line
+    return remainder;
+}
+
+console.log(main(10, 3));   //change this line
+
+module.exports = main;
