fc5385772ba4ced8a2c0ef33a43ca1911cef167b
unknown <edina.kurtic90@hotmail.com>
2021-03-11 22:21:19 +0100

    Dodana ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,13 @@
+function main (numOne, numTwo){
+    //only change code below this line
+    
+    var myQuotient = numOne / numTwo;
+
+    //only change code below this line
+    return myQuotient;
+}
+
+console.log(main(5.0, 2.0));   //change this line
+console.log(main(16.5, 5.5));   //change this line
+console.log(main(102.0, 25.5));  //change this line
+module.exports = main;
