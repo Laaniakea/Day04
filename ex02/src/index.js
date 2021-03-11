b0c2102e0106192b11981785de18ed955ba1c7c2
unknown <edina.kurtic90@hotmail.com>
2021-03-11 13:32:45 +0100

    Dodadn ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,17 @@
+function main(){
+
+    var myRes = 75;
+    
+    //change code below this line
+
+    myRes++;
+
+    //change code above this line
+
+    return myRes;
+
+}
+
+console.log(main());
+
+module.exports = main;
