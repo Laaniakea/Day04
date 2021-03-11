f251e2c247db1b828c8cf6c4e14c57a8b8c66aa1
unknown <edina.kurtic90@hotmail.com>
2021-03-11 14:23:34 +0100

    ex04 was added

---
+++ ex04/src/index.js
@@ -0,0 +1,12 @@
+function main(){
+     // only change code below this line
+    var myDecimal = 8.2;
+    var myDecimalE = 2.54 * 10e-4;
+    // only change code below this line
+    return {
+        myDecimal,
+        myDecimalE
+    };
+}
+console.log(main());
+module.exports = main;
