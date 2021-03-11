c725240fadf55d54766334106d6039861ca6c940
unknown <edina.kurtic90@hotmail.com>
2021-03-12 00:33:18 +0100

    ex07 finding a Reminder,edited

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,14 +1,14 @@
 function main (numOne, numTwo){
-    //only change code below this line
+    // only change code below this line
     
     var remainder = numOne % numTwo;
 
-    //only change code below this line
+    // Only change code below this line
     return remainder;
 }
 
-console.log(main(5, 2));   //change this line
-console.log(main(15, 4));   //change this line
-console.log(main(102, 25));   //change this line
+console.log(main(5, 2));   // Change this line
+console.log(main(15, 4));   // Change this line
+console.log(main(102, 25));   // Change this line
 
 module.exports = main;
