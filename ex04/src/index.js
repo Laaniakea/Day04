05060c63cffb2d82b394ce9890bfbdf05db444a1
unknown <edina.kurtic90@hotmail.com>
2021-03-12 00:02:46 +0100

    Decimal Numbers

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,8 +1,11 @@
 function main(){
+
      // only change code below this line
+
     var myDecimal = 8.2;
-    var myDecimalE = 2.54 * 10e-4;
-    // only change code below this line
+    var myDecimalE = 2.54 * 1e-3;
+
+    // Only change code below this line
     return {
         myDecimal,
         myDecimalE
