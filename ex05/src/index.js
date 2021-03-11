7f02e9cdcc737f01d6bd2f2be7f6054fabfb0066
unknown <edina.kurtic90@hotmail.com>
2021-03-12 00:30:19 +0100

    ex06, Divide One decimal by Another,edited

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -1,14 +1,15 @@
 function main (numOne, numTwo){
-    //only change code below this line
+
+    // only change code below this line
     
     var product = numOne * numTwo;
 
-    //only change code below this line
+    // Only change code below this line
     return product;
 }
 
-console.log(main(2.5, 2.0));   //change this line
-console.log(main(5.5, 5.5));
-console.log(main(100.25, 43.20));
+console.log(main(2.5, 2.0));   // Change this line
+console.log(main(5.5, 5.5));   // Change this line 
+console.log(main(100.25, 43.20));  // Change this line
 module.exports = main;
 
